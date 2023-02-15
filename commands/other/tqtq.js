9474df01a1638b2ba8174e79497cbb292ad68bec
armand <106297716+armnd-a@users.noreply.github.com>
2023-02-15 17:01:28 +0700

    Create tqtq.js

---
+++ commands/other/tqtq.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'tqtq',
+    cmd: ['tqtq', 'infosc', 'infosc'],
+    category: 'other',
+    async handler(m, {conn}){
+     txt = `*SOURCE*\n`
+     txt += `Sc : https://github.com/SkylarKaf/elaina-md\n`
+     txt += `Di sc public beberapa fitur langkah di hapus!\n`
+     txt += `untuk menghindari kang copas no cr\n\n`     
+     txt += `*Tq credits*\n`
+     txt += `armand\n• Rzkyfdlh\n• Zynfinity\n• SkylarKaf\n• Senkuu\n• ZeraID`    
+     m.reply(txt)
+    }
+}
